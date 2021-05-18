2035a73ec7cafb3cb992b402d0167d145cb79ab1
Naman Singla <75626821+nsingla20@users.noreply.github.com>
2021-05-18 18:03:45 +0530

    Update

--- coins.js
+++ coins.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 var PORT = 8080;
-  
+const fs = require('fs');
+const data=require("./data.json");
 app.use(express.json());
     
 app.post('/coins', function (req, res) {
-    if(req.body.rollno=="200619")res.end('{"coins":infinite}');
-    else res.end('{"coins":'+parseInt(req.body.rollno)+'}');
+    try{
+        if(req.body.rollno in data.coins){
+            res.end('{"coins":'+data.coins[req.body.rollno]+'}');
+        }else throw "roll no Not_FOUND";
+    }catch(err){
+        res.end(err);
+    }
 })
   
 app.listen(PORT, function(err){
