cf70a596b26b401740de524a819117a82b79f45c
Naman Singla <75626821+nsingla20@users.noreply.github.com>
2021-05-18 16:15:13 +0530

    added ja file

---
+++ coins.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var PORT = 8080;
+  
+app.use(express.json());
+    
+app.post('/', function (req, res) {
+    if(req.body.rollno=="200619")res.end('{"coins":infinite}');
+    else res.end('{"coins":'+parseInt(req.body.rollno)+'}');
+})
+  
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
